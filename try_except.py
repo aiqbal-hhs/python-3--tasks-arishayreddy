b733c748b81e7bb5eb95ba935ee9d8e05e121ccf
Arishay Reddy <64936362+arishayreddy@users.noreply.github.com>
2022-03-08 14:25:35 +1300

    Update try_except.py

--- try_except.py
+++ try_except.py
@@ -1,6 +1,14 @@
 while True:
-  try
-  number = int(input("Enter a number: "))
-  break
+  try:
+    number = int(input("Enter a number: "))
+    break
   except ValueError:
-print("Please enter a number!")
+    print("Please enter a number!")
+
+
+while True:
+  try:
+    height = float(input("Enter your height in meters: "))
+    break
+  except ValueError:
+    print("Please enter a valid height in meters eg. 1.65")
